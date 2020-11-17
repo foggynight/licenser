d73377cc471d7bc2552d7fe1285b065a8b3a86db
foggynight <foggynight709@gmail.com>
2020-11-17 07:11:01 -0330

    Add process_args function

--- licenser.py
+++ licenser.py
@@ -8,6 +8,32 @@
 # Copyright (C) 2020 Robert Coffey
 
 from pathlib import Path
+from sys import argv
+
+def process_args():
+    """
+    Process the command line arguments, getting the license filepath,
+    preamble filepath, and a list of file extensions.
+
+    Should either argument be omitted, it will be gotten from an input
+    prompt in the program.
+
+    @returns:
+    - license {str|None}: Path to the license file
+    - preamble {str|None}: Path to the preamble file
+    - exts {[str]|None}: A list of file extensions
+    """
+    license = preamble = exts = None
+    argc = len(argv)
+    if argc > 3:
+        exts = []
+        for i in range(argc-3):
+            exts.append(argv[i+3])
+    if argc > 2:
+        preamble = open(argv[2]).readlines()
+    if argc > 1:
+        license = open(argv[1]).readlines()
+    return license, preamble, exts
 
 def get_text(msg):
     """
@@ -28,20 +54,18 @@ def get_text(msg):
     return file.readlines()
 
 if __name__ == '__main__':
-    # Get license and preamble
-    license = get_text('License path: ')
-    preamble = get_text('Preamble path: ')
+    license, preamble, exts = process_args()
 
-    # Get list of target source files
-    exts = input('File extensions: ').split()
+    if not license: license = get_text('License path: ')
+    if not preamble: preamble = get_text('Preamble path: ')
+    if not exts: exts = input('File extensions: ').split()
+
     targs = []
     for ext in exts:
         targs.extend([str(e) for e in Path('.').glob(f'**/*.{ext}')])
 
-    # Copy license to target dir
     open('LICENSE', 'w').writelines(license)
 
-    # Copy file preamble to all source files
     for file in targs:
         old = open(file).readlines()
         open(file, 'w').writelines(preamble + old)
