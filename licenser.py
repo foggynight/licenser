8ec7390e2a8d0485b8e2c9b941124c490cf4f327
foggynight <foggynight709@gmail.com>
2020-11-17 04:21:33 -0330

    Add main function

---
+++ licenser.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0
+
+# --- licenser ---
+#
+# Import a license into source code.
+#
+# Copyright (C) 2020 Robert Coffey
+
+def main():
+    # get desired license
+    # get desired file preamble
+    # get list of target source files
+    # copy license to target dir
+    # copy file preamble to all source files
+    pass
+
+if __name__ == '__main__':
+    main()
